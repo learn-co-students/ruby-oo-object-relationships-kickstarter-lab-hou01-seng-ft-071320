79fa40284814c9e39b52fc8b592d0d76ed611196
Abbosbek-SE <muydinboevabbosbek@gmail.com>
2020-07-26 19:29:15 -0700

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project 
+    
+    attr_reader :title 
+
+    def initialize(title)
+        @title = title 
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers 
+        project_backers = ProjectBacker.all.select {|i| i.project == self}
+        project_backers.map {|i| i.backer}
+    end
+
+end
